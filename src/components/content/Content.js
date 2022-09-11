7d1f18d5af7d9172320d485618de9302fc9e480d
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-11 15:51:16 +0430

    components initialized

---
+++ src/components/content/Content.js
@@ -0,0 +1,9 @@
+const Content = () => {
+    return <div className="p-5">
+        <p>
+        
+        </p>
+    </div>
+}
+ 
+export default Content;
---
+++ src/components/scrollProgress/ScrollProgress.js
@@ -0,0 +1,5 @@
+const ScrollProgress = () => {
+    return <section></section>
+}
+ 
+export default ScrollProgress;
