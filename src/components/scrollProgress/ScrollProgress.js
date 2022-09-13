f71b6e769a0976c26e66329fcdee7ce06622c77d
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-13 09:31:09 +0430

    progress style added

--- src/components/scrollProgress/ScrollProgress.js
+++ src/components/scrollProgress/ScrollProgress.js
@@ -1,5 +1,31 @@
+import './ScrollProgress.css'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const ScrollProgress = () => {
-    return <section></section>
+    const [scroll , setscroll] = useState(0);
+
+    const scrollprogress= ()=>{
+        const scrollPx = document.documentElement.scrollTop;
+        const winHeightPx = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrollWidth = `${scrollPx/winHeightPx*100}%`;
+        setscroll(scrollWidth);
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", scrollprogress);
+        
+        return ()=>{
+            window.removeEventListener("scroll", scrollprogress);
+        }
+
+    })
+
+    return <section>
+        <div className='scroll-static'>
+            <div className='scroll-dynamic' style={{width:scroll}}></div>
+        </div>
+    </section>
 }
  
 export default ScrollProgress;
